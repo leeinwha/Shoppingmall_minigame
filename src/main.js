7c6fc4015e3f13702d329884812441e286cfdd38
leeinwha <88917793+leeinwha@users.noreply.github.com>
2022-01-05 15:16:20 +0900

    Javascript

--- src/main.js
+++ src/main.js
@@ -1,1 +1,31 @@
-"use strick"
+// Fetch the items from the JSON file
+function loadItems() {
+  return fetch('data/data.json')
+  .then(response => response.json())
+  .then(json => json.items);
+}
+
+// Update the list with the given items
+function displayItems(items) {
+  const container = document.querySelector('.items');
+  const html = items.map(item => createHTMLString(item));
+  container.innerHTML = items.map(item => createHTMLString(item)).join('');
+}
+
+// Create HTML List item from the given data item
+function createHTMLString(item) {
+  return`
+    <li class="item">
+      <img src="${item.image}" alt="${item.type}" class="item__thumbnail">
+      <span class="item_descroption">${item.gender}, ${item.size} </span>
+    </li>
+  `;
+}
+
+// main
+loadItems()
+  .then(items => {
+    displayItems(items);
+    // setEventListeners(items)
+  })
+  .catch(console.log);
